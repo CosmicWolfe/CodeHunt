834a830cf2615b570d9c20ff89ded1ddf961ad7e
Jamie Sebastian <jamiesebastianjo@gmail.com>
2020-06-11 00:28:59 -0400

    add general structure of components

--- codehunt/src/app/app.module.ts
+++ codehunt/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { QuestionsTableComponent } from './questions-table/questions-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    QuestionsTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ codehunt/src/app/questions-table/questions-table.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questions-table',
+  templateUrl: './questions-table.component.html',
+  styleUrls: ['./questions-table.component.scss']
+})
+export class QuestionsTableComponent implements OnInit {
+  // will take as input user handle, and filters
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
