a93a9c7675bfc5c7cb39aa58006c4a0de1fbb5d6
Jamie Sebastian <jamiesebastianjo@gmail.com>
2020-06-23 17:44:39 -0400

    make sorting available from column

--- codehunt/src/app/questions-table/questions-table.component.ts
+++ codehunt/src/app/questions-table/questions-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, SimpleChange, Input, ViewChild } from '@angular/core';
 import { MatTableModule, MatTable } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Question } from '../app.constants';
 
@@ -13,7 +13,7 @@ import { Question } from '../app.constants';
 
 export class QuestionsTableComponent implements OnInit {
   // will take as input user handle, and filters
-  @Input('questions') questions : []
+  @Input('questions') questions : Question[]
 
   displayedColumns: string[] = ['No.', 'index', 'name', 'solvedCount', 'rating'];
   dataSource = new MatTableDataSource<Question>(this.questions);
@@ -25,6 +25,29 @@ export class QuestionsTableComponent implements OnInit {
   ngOnInit(): void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    this.dataSource.sortData = (data: Question[], sort: MatSort) => {
+      let sortedData = data.slice();
+      const filters = {
+        ascending: this.sort.direction == 'asc',
+        sortBy: this.sort.active
+      };
+      sortedData.sort((q1: Question, q2: Question) => {
+        let sortDirection = filters.ascending ? 1 : -1;
+        if (filters.sortBy == 'rating') {
+          if (q2.rating != q1.rating) {
+            return (q2.rating - q1.rating) * sortDirection;
+          }
+          return q2.solvedCount - q1.solvedCount;
+        } else if (filters.sortBy == 'index') {
+          if (q1.contestId == q2.contestId) {
+            return q1.problemId.localeCompare(q2.problemId) * sortDirection;
+          }
+          return ((q2.contestId) - (q1.contestId)) * sortDirection;
+        }
+        return (q2.solvedCount - q1.solvedCount) * sortDirection;
+      });
+      return sortedData;
+    };
   }
 
   ngOnChanges(changes : SimpleChange): void {
@@ -32,5 +55,4 @@ export class QuestionsTableComponent implements OnInit {
     this.dataSource.data = this.questions;
     this.dataSource.paginator = this.paginator;
   }
-
 }
